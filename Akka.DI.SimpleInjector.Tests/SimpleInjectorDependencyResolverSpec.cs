dd888f67ac296c82b2dcfbf1ff8a4bd3d2114338
Carlos Torrecillas <carlos.torrecillas@just-quality.com>
2016-02-15 19:59:14 +0000

    Added Akka.DI.SimpleInjector and tests

---
+++ Akka.DI.SimpleInjector.Tests/SimpleInjectorDependencyResolverSpec.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Akka.Actor;
+using Akka.DI.Core;
+using Akka.DI.TestKit;
+using SimpleInjector;
+using SimpleInjector.Extensions.ExecutionContextScoping;
+using Xunit;
+
+namespace Akka.DI.SimpleInjector.Tests
+{
+    public class SimpleInjectorDependencyResolverSpec : DiResolverSpec
+    {
+        protected override object NewDiContainer()
+        {
+            var container =  new Container();
+
+            container.Options.DefaultScopedLifestyle = new ExecutionContextScopeLifestyle();
+
+            return container;
+        }
+
+        protected override IDependencyResolver NewDependencyResolver(object diContainer, ActorSystem system)
+        {
+            var container = ToContainer(diContainer);
+
+            return new SimpleInjectorDependencyResolver(container, system);
+        }
+ 
+        protected override void Bind<T>(object diContainer, Func<T> generator)
+        {
+            var container = ToContainer(diContainer);
+
+            container.Register(typeof(T), () => generator(), Lifestyle.Scoped);
+        }
+ 
+        protected override void Bind<T>(object diContainer)
+        {
+            var container = ToContainer(diContainer);
+
+            container.Register(typeof(T), typeof(T), Lifestyle.Scoped);
+        }
+ 
+        private static Container ToContainer(object diContainer)
+        {
+            var container = diContainer as Container;
+
+            Assert.True(container != null, "container != null");
+
+            return container;
+        }
+    }
+}
