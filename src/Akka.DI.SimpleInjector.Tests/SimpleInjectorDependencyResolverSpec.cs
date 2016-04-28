a0be6bed943868805327a2213cf7c70df646f65c
Marc Piechura <piechura@gmx.net>
2016-04-28 11:42:29 +0200

    Update to 1.0.8 and fixed nuget bug

--- src/Akka.DI.SimpleInjector.Tests/SimpleInjectorDependencyResolverSpec.cs
+++ src/Akka.DI.SimpleInjector.Tests/SimpleInjectorDependencyResolverSpec.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="WindsorDependencyResolver.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2016 Akka.NET project <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
 using Akka.Actor;
 using Akka.DI.Core;
 using Akka.DI.TestKit;
